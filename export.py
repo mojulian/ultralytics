dc126eb46194ee69b93087a0ac5912efe06290ca
Julian Moosmann <mojulian@vilan1.ee.ethz.ch>
2023-07-21 13:57:55 +0200

    init export file

---
+++ export.py
@@ -0,0 +1,19 @@
+# import pdb, 
+import os
+import torch
+import numpy as np
+from ultralytics import YOLO
+
+import torch.nn as nn
+from ultralytics.nn.tasks import get_size
+
+from ultralytics import YOLO
+
+version_name="5"
+model_name="yolov"
+scale="p"
+# model = YOLO(f'ultralytics/cfg/models/v{version_name}/{model_name}.yaml')  # load a pretrained model (recommended for training)
+model = YOLO(f'{model_name}{version_name}{scale}.yaml')  # load a pretrained model (recommended for training)
+
+# Train the model
+model.train(data='VOC.yaml', epochs=1000, imgsz=352)
