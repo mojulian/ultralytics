599df6b807597a0b20c967ef5ed3371a6b0b09f6
Julian Moosmann <mojulian@vilan1.ee.ethz.ch>
2023-07-21 14:19:07 +0200

    init export file

--- export.py
+++ export.py
@@ -14,6 +14,5 @@ model_name="yolov"
 scale="p"
 # model = YOLO(f'ultralytics/cfg/models/v{version_name}/{model_name}.yaml')  # load a pretrained model (recommended for training)
 model = YOLO(f'{model_name}{version_name}{scale}.yaml')  # load a pretrained model (recommended for training)
-
 # Train the model
 model.train(data='VOC.yaml', epochs=1000, imgsz=352)
