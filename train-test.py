8ce27797a60ca6c15d63b2003c43acb89f9a7f54
Julian Moosmann <mojulian@vilan1.ee.ethz.ch>
2023-07-21 13:57:37 +0200

    adding wandb support

--- train-test.py
+++ train-test.py
@@ -6,14 +6,28 @@ from ultralytics import YOLO
 
 import torch.nn as nn
 from ultralytics.nn.tasks import get_size
+from wandb.integration.yolov8 import add_callbacks as add_wandb_callbacks
 import yaml
 
+import wandb
+
+# wandb.init()
+# run = wandb.init(
+#     # Set the project where this run will be logged
+#     project="ultralytics-gapyolo",
+#     )
 
 from ultralytics import YOLO
 
 # Load a model
 # model = YOLO('runs/detect/train10/weights/last.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov5p.yaml')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov5p.yaml')  # load a pretrained model (recommended for training)
+version_name="5"
+model_name="yolov"
+scale="p"
+# model = YOLO(f'ultralytics/cfg/models/v{version_name}/{model_name}.yaml')  # load a pretrained model (recommended for training)
+model = YOLO(f'{model_name}{version_name}{scale}.yaml')  # load a pretrained model (recommended for training)
+add_wandb_callbacks(model, project =f"gapyolo-{model_name}{scale}-{version_name}")
 
 # Train the model
-model.train(data='VOC.yaml', epochs=100, imgsz=640, resume=True)
+model.train(data='VOC.yaml', epochs=1000, imgsz=352)
